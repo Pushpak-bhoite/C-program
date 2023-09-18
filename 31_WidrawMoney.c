79d694499adff0affdc7b18655cc92d751f129af
Pushpak bhoite <bhoitepushpak6@gmail.com>
2023-09-18 23:18:02 +0530

    todays sec comm

---
+++ 31_WidrawMoney.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int amt,tamt = 2000;
+    float bal;
+    printf("Enter The Amt :\t");
+    scanf("%d", &amt);
+    if (amt%5==0&&amt< (tamt-1))
+    {
+         bal =(float)tamt - (amt + 0.5);
+
+        printf("your transaction is successfully Your rem balnce is %f", bal);
+    }else{
+                printf("you Dont Have sufficient Balnce  %d", tamt);
+
+    }
+}
