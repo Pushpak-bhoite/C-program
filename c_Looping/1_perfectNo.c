ef1f8af7f61b150731728c29d08b32d406a68a23
Pushpak bhoite <bhoitepushpak6@gmail.com>
2023-09-23 22:39:05 +0530

    1st ,2nd and 3rd chapter sorted

---
+++ c_Looping/1_perfectNo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int no = 6, i = 1, per=0;
+    while (i < no)
+    {
+        if (no % i == 0)
+        {
+            per = per + i;
+            printf("%d",i);
+        }
+        i++;
+    }
+    if (per == no)
+    {
+        printf("\nYES");
+    }
+    else
+    {
+        printf("\n\nNO");
+    }
+    return 0;
+}
