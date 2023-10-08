92193e86cd6705b2ecc22d4d7424a78f01973d49
Pushpak bhoite <bhoitepushpak6@gmail.com>
2023-10-09 01:06:14 +0530

    todays commit

--- c_Looping/hello.c
+++ c_Looping/hello.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
+/*
+    0         0
+    01       01
+    010     010
+    0101
+    01010
+
+*/
 int main()
 {
-    int no = 6,i=4 ,sum=0;
-    int a=no%i;
-    printf("%d",a);
-    
+    int i, j, flag = 1;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 10; j++)
+        {
+            if (j <= i || j + i >= 11)
+            {
+                if (j <= 5 && j % 2)
+                {
+                    printf("%d", 0);
+                }
+                else if (j > 5 && flag)
+                {
+                    printf("%d", 0);
+                    flag = 0;
+                }
+                else
+                {
+                    printf("%d", 1);
+                    flag = 1;
+                }
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
 }
